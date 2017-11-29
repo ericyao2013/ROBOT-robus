f355f947be76e3df2ceea0555711df5b67c95099
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-11-29 09:14:08 +0100

    Add legacy module type.

--- src/module/mod_type.rs
+++ src/module/mod_type.rs
@@ -1,5 +1,18 @@
 /// Available `Module` type
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum ModuleType {
+    Gate,
+    Servo,
+    RgbLed,
+    Potentiometer,
     Button,
+    DistanceSensor,
+    Relay,
+    DynamixelMotor,
+    Stepper,
+    HomeMadeServo,
+    Ledstrip,
+    Rtc,
+    Encoder,
+    GenericMotor,
 }
