532d681816e5d85da299f1f0141f7f8163897cf8
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-12-01 16:27:06 +0100

    Update readme.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Robus: the Robust Robotic Bus
+
+Robus lets you seamlessly connect robotic actuators and sensors using a unified bus. New modules are automatically detected thanks to the topology detection algorithm.
+
+Each robus node is called a module and represent a function in your robot - typically a sensor or actuator. Modules can directly communicate with other modules using broadcast, specific module ids or specific module groups.
+
+Robus is light-weighted and designed for the embedded world.
+
+Robus reduces the time between an idea to the prototype. It provides a unified messaging architecture for modular robotics, all modules can be connected on the same 5 wires bus containing both power and 2 communication bus.
--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@
 //!
 //! Each robus node is called a module and represent a function in your robot - typically a sensor or actuator. Modules can directly communicate with other modules using broadcast, specific module ids or specific module groups.
 //!
-//! Robus is lightweighted and designed for the embedded world.
+//! Robus is light-weighted and designed for the embedded world.
 //!
-//! Robus reduces the time between an idea to the prototype. It provides a unified messaging achitecture for modular robotics, all modules can be connected on the same 5 wires bus containing both power and 2 communication bus.
+//! Robus reduces the time between an idea to the prototype. It provides a unified messaging architecture for modular robotics, all modules can be connected on the same 5 wires bus containing both power and 2 communication bus.
 
 #![no_std]
 #![feature(alloc)]
