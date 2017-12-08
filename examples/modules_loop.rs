1ef00dbf677c4e98bb9aa0cee0d145ed9bddbf16
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-12-08 14:03:27 +0100

    Add log in the module loop.

--- examples/modules_loop.rs
+++ examples/modules_loop.rs
@@ -29,6 +29,9 @@ extern "C" {
 extern crate alloc;
 use alloc::vec::Vec;
 
+use core::fmt::Write;
+
+#[macro_use]
 extern crate robus;
 
 use robus::{Command, Message, ModuleType};
@@ -60,11 +63,16 @@ fn main() {
     if ID == 1 {
         hal::rcc::ms_delay(1000);
         core.send(module, &mut send_msg);
+        log!("Start!");
     }
 
     loop {
         if let Some(_) = rx.recv() {
             core.send(module, &mut send_msg);
+
+            if ID == 1 {
+                log!("Loop!");
+            }
         }
     }
 }
