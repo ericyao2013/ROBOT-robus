443b0cdc8d4f40e48738eea4779afe44f59068d6
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2018-01-15 15:25:21 +0100

    add mod_type functions

--- src/module/mod_type.rs
+++ src/module/mod_type.rs
@@ -30,4 +30,32 @@ impl ModuleType {
             _ => false,
         }
     }
+    pub fn as_str(&self) -> &str {
+        match *self {
+            ModuleType::Gate => "gate",
+            ModuleType::Servo => "servo",
+            ModuleType::RgbLed => "led",
+            ModuleType::Potentiometer => "potard",
+            ModuleType::Button => "button",
+            ModuleType::DistanceSensor => "distance",
+            ModuleType::Relay => "relay",
+            ModuleType::DynamixelMotor => "dynamixel",
+            ModuleType::Stepper => "stepper",
+            ModuleType::Encoder => "encoder",
+            ModuleType::InputGPIO => "gpio",
+            _ => panic!("unsupported module type!"),
+        }
+    }
+    pub fn as_field(&self) -> &str {
+        match *self {
+            ModuleType::DistanceSensor => "distance",
+            ModuleType::Button | ModuleType::InputGPIO => "state",
+            ModuleType::Potentiometer
+            | ModuleType::Encoder
+            | ModuleType::DynamixelMotor
+            | ModuleType::HomeMadeServo
+            | ModuleType::Stepper => "position",
+            _ => panic!("unsupported module type!"),
+        }
+    }
 }
